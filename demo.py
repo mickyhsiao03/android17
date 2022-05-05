3e64d3765d8189864de4f841cf8600a164ddea77
Shih Chieh Hsiao <mickyhsiao03@hotmail.com>
2022-05-05 13:22:38 -0700

    updated input format

--- demo.py
+++ demo.py
@@ -2,13 +2,13 @@ import calculate
 
 user_name = input('Enter user name: ')
 course = input('Enter course name: ')
-quiz = int(input('Enter your quiz mark'))
-lab = int(input('Enter your labs mark'))
-assignment = int(input('Enter your assignments and project marks'))
-presentation = int(input('Enter your presentation marks'))
-participation = int(input('Enter your participation marks'))
-midterm = int(input('Enter your midterm marks'))
-final = int(input('Enter your final exam mark'))
+quiz = int(input('Enter your quiz mark: '))
+lab = int(input('Enter your labs mark: '))
+assignment = int(input('Enter your assignments and project marks: '))
+presentation = int(input('Enter your presentation marks: '))
+participation = int(input('Enter your participation marks: '))
+midterm = int(input('Enter your midterm marks: '))
+final = int(input('Enter your final exam mark: '))
 
 grades = calculate.calculate(user_name, course, quiz, lab, assignment, presentation, participation, midterm, final)
 
